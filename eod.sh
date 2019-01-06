f660b091bd43951312fd83f39b6031496d3bde46
lasithh <lasithh@gmail.com>
2019-01-06 10:49:40 -0800

    Daily Update

--- eod.sh
+++ eod.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/expect
 
 #Download latest data
-#curl -v http://127.0.0.1:8000/loadLatestData
+curl -v http://127.0.0.1:8000/loadLatestData
 
 #Git commit and push
 git add -A
-
-#expect "login"
-#Authenticate
-#send "lasithh"
-#cat << "gela1985@git"
 
 #Commit
 git commit -a -m "Daily Update"
