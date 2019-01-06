26f95665270a04cd0b4da38160c08bd1aad47ad0
lasithh <lasithh@gmail.com>
2019-01-06 10:40:12 -0800

    Daily Update

--- eod.sh
+++ eod.sh
@@ -1,9 +1,16 @@
-#!/bin/bash
+#!/usr/bin/expect
 
 #Download latest data
-curl -v http://127.0.0.1:8000/loadLatestData
+#curl -v http://127.0.0.1:8000/loadLatestData
 
 #Git commit and push
 git add -A
+
+#expect "login"
+#Authenticate
+#send "lasithh"
+#cat << "gela1985@git"
+
+#Commit
 git commit -a -m "Daily Update"
 git push
--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/computer/project/MyProject1
+cd /home/computer/workspace/MyProject1
 
 #Start the service
 python manage.py runserver 0.0.0.0:8000 > output
