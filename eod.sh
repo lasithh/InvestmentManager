11122d9c82b511c95fe758f51d18b29e00b255e9
Lasith <lasithh@gmail.com>
2019-06-21 07:14:31 +0100

    daily update

--- eod.sh
+++ eod.sh
@@ -4,7 +4,11 @@
 git pull
 
 #Download latest data
-#curl -v http://127.0.0.1:8000/loadLatestData
+echo "loading eod data"
+curl -v http://127.0.0.1:8000/loadLatestData
+
+echo "Load financial Tables"
+curl -v http://127.0.0.1:8000/extractFinancialReports
 
 #Git commit and push
 git add -A
