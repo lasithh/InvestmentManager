55efd69a962642601cd1e5fcae1e9f3a4e3cbf8f
Lasith Haputhanthiri <hapu@amazon.com>
2019-06-23 23:29:56 -0700

    logs

--- MyInvestementsManager/ReportProcessor/reports_store.py
+++ MyInvestementsManager/ReportProcessor/reports_store.py
@@ -4,7 +4,7 @@ from MyInvestementsManager.models import ReportTables
 
 
 def store_report_tables(company, date, name, tables):
-    print ("Storing: company " + company.symbol + " name: " + name)
+    print ("Storing: company " + company.symbol + " name: " + name + "date: " + str(date))
     ReportTables.objects.get_or_create(company=company,
                                        date = datetime.datetime.fromtimestamp(date).strftime('%Y-%m-%d %H'),
                                        name = name,
