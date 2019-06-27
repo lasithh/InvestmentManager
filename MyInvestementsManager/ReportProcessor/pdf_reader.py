4290600b661bb228056affea241487b2bc162671
Lasith Haputhanthiri <hapu@amazon.com>
2019-06-26 22:25:07 -0700

    bug fix

--- MyInvestementsManager/ReportProcessor/pdf_reader.py
+++ MyInvestementsManager/ReportProcessor/pdf_reader.py
@@ -6,22 +6,21 @@ from tabula import read_pdf
 
 def extract_tables(file_path):
     output = dict()
-    fh = open(file_path, mode='rb')
-    reader = pyPdf.PdfFileReader(fh)
-    n = reader.getNumPages()
-    fh.close()
-    for i in range(n):
-        try:
+    try:
+        fh = open(file_path, mode='rb')
+        reader = pyPdf.PdfFileReader(fh)
+        n = reader.getNumPages()
+        fh.close()
+        for i in range(n):
             df = read_pdf(file_path, pages=str(i + 1), multiple_tables=True)
-        except Exception as e:
-            print("Ignored error occured during tables extraction")
-            print traceback.format_exc()
-
-        if df:
-            list_of_tables = list()
-            for df_table in df:
-                list_of_tables.append(df_table.to_json())
-            output[i] = list_of_tables
+            if df:
+                list_of_tables = list()
+                for df_table in df:
+                    list_of_tables.append(df_table.to_json())
+                output[i] = list_of_tables
+    except Exception as e:
+        print("Ignored error occured during tables extraction")
+        print traceback.format_exc()
 
 
     return output
